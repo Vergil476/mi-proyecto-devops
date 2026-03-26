3b711d481554d64b117f36067c51598cb612ddaf
carlos-perez-1616 <carlosaperezvazquez@gmail.com>
2026-03-26 17:43:27 -0500

    validate_project.py  actualizacion

--- automation/validate_project.py
+++ automation/validate_project.py
@@ -1,5 +1,19 @@
 import os
 import sys
+from flask import Flask, render_template
+import index.html as html
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Busca 'index.html' en la carpeta 'templates'
+    return render_template('index.html') 
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 errors = []
 
@@ -11,6 +25,14 @@ if not os.path.exists("src/styles.css"):
 
 if not os.path.exists("README.md") or os.path.getsize("README.md") == 0:
     errors.append("README.md no existe o está vacío")
+
+
+if html.count("<h1>") < 1:
+    errors.append("index.html debe contener un h1")
+
+if html.count("<p>") < 1:
+    errors.append("index.html debe contener al menos un párrafo")
+
 
 if errors:
     print("Errores encontrados:")
